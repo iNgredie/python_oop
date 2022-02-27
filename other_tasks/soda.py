91b320f4b8f201c99e2c98e459507ed41ac0b394
Aleksandr Durnev <sandslash@bk.ru>
2022-02-27 22:34:36 +0400

    add soda

---
+++ other_tasks/soda.py
@@ -0,0 +1,31 @@
+"""
+Создайте класс Soda (для определения типа газированной воды), принимающий 1 аргумент при инициализации
+(отвечающий за добавку к выбираемому лимонаду). В этом классе реализуйте метод show_my_drink(),
+выводящий на печать «Газировка и {ДОБАВКА}» в случае наличия добавки, а иначе отобразится следующая
+фраза: «Обычная газировка».
+"""
+
+
+class Soda:
+
+    topping: str
+
+    def __init__(self, ingredient: str = None):
+        if isinstance(ingredient, str):
+            self.ingredient = ingredient
+        else:
+            self.ingredient = None
+
+    def show_my_drink(self):
+        if self.ingredient:
+            print(f'Газировка и {self.ingredient}')
+        else:
+            print('Обычная газировка')
+
+
+drink1 = Soda()
+drink2 = Soda('малина')
+drink3 = Soda(5)
+drink1.show_my_drink()
+drink2.show_my_drink()
+drink3.show_my_drink()
